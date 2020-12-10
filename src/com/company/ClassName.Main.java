755eb2b559d1e87ccda2c968d79392d120f50def
Jakyrin Robinson <jakyrin.robinson@bison.howard.edu>
2020-12-10 13:51:08 -0500

    Initial commit

---
+++ src/com/company/ClassName.Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ClassName {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hi, How's your day?");
+        String n = input.nextLine();
+    }
+}
